99ccc00c0941de1e154e89165f34d0fc6075a298
Dang Van Thanh <dangvanthanh@dangthanh.org>
2021-10-03 17:11:16 +0700

    Introduction rust

---
+++ rust/app/README.md
@@ -0,0 +1,10 @@
+# Rust
+
+## CLI
+
+```
+$ cargo run
+$ cargo run --bin app
+$ cargo run --release
+$ API_TOKEN=<token> cargo run -- vietnam
+```
---
+++ rust/app/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("Hello, world!");
+    let _api_token = std::env::var("API_TOKEN")
+        .expect("expected there to be an api token");
+
+    let mut arg_iterator = std::env::args();
+    arg_iterator.next();
+    let args: String = arg_iterator.collect();
+
+    dbg!(args);
+}
