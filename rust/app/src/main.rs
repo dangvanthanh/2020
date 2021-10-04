984e632c49d2d90784576cfd31aa77375846827d
Dang Van Thanh <dangvanthanh@dangthanh.org>
2021-10-04 23:50:11 +0700

    Make http request in rust

--- rust/app/src/main.rs
+++ rust/app/src/main.rs
@@ -1,11 +1,21 @@
 fn main() {
     println!("Hello, world!");
-    let _api_token = std::env::var("API_TOKEN")
+    let api_token = std::env::var("API_TOKEN")
         .expect("expected there to be an api token");
 
     let mut arg_iterator = std::env::args();
     arg_iterator.next();
     let args: String = arg_iterator.collect();
 
-    dbg!(args);
+    let client = reqwest::blocking::Client::new();
+
+    let response = client
+        .get("https://api.waqi.info/search/")
+        .query(&[("token", api_token), ("keyword", args)])
+        .send()
+        .expect("a successful request")
+        .json::<serde_json::Value>()
+        .expect("expect the body to be json");
+
+    println!("{}",response);
 }
